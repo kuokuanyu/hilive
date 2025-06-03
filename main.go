0699c13dceac147d1a50ca959b9600dbf6f01703
kuokuanyo <53857275+kuokuanyo@users.noreply.github.com>
2025-06-03 09:11:08 +0800

    9

--- main.go
+++ main.go
@@ -8,14 +8,8 @@ import (
 
 	_ "github.com/go-sql-driver/mysql" // mysql引擎
 )
-<<<<<<< HEAD
-123456789
-=======
-111111111111111111111111
+888888888
 
-
-3333333333333333333333333
->>>>>>> 8
 // @title Hilives API 文檔
 // @version 1.0
 // @description Hilives 平台
